35cdfc52425aa544732ec65a1916cfa6b83250cc
anjub95 <56960143+anjub95@users.noreply.github.com>
2019-10-25 14:22:40 +0530

    file for printing file type

--- vars/demoTool.groovy
+++ vars/demoTool.groovy
@@ -20,6 +20,7 @@ def downloadArtifact(config, toolsArtifactPath){
     
     def out = './tools/oracle-jdk'
         sh 'mkdir -p ./tools/oracle-jdk'
+        sh 'file /var/lib/jenkins/workspace/SharedLibrary_pipeline/tools/oracle-jdk/jdk-8u131-linux-x64.tar.gz'
     sh 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P '+ out
         
     //sh 'curl -L http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -o ' + out
