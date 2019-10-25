220042b8d47318d9603cc49959b85cd3a86cd4b0
anjub95 <56960143+anjub95@users.noreply.github.com>
2019-10-25 12:21:29 +0530

    removing quotes

--- vars/demoTool.groovy
+++ vars/demoTool.groovy
@@ -20,7 +20,7 @@ def downloadArtifact(config, toolsArtifactPath){
     
     def out = './tools/oracle-jdk'
         sh 'mkdir -p ./tools/oracle-jdk'
-    sh 'wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz" -P '+ out
+    sh 'wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P '+ out
         
     //sh 'curl -L http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -o ' + out
         /* def downloadSpec = JsonOutput.toJson([
