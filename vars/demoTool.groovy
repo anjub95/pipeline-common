f433af54b0e1708e724a26b1ed390cfd23b85528
anjub95 <56960143+anjub95@users.noreply.github.com>
2019-10-25 14:49:37 +0530

    Listing directories

--- vars/demoTool.groovy
+++ vars/demoTool.groovy
@@ -21,6 +21,8 @@ def downloadArtifact(config, toolsArtifactPath){
     def out = './tools/oracle-jdk'
         sh 'mkdir -p ./tools/oracle-jdk'
         sh 'file ./tools/oracle-jdk/jdk-8u131-linux-x64.tar.gz'
+        sh 'cd tools/oracle-jdk'
+        sh 'ls -ltr'
     sh 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P '+ out
         
     //sh 'curl -L http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -o ' + out
