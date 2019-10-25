790a06e315ccf40095e84d1547830bc2cf8c21a7
anjub95 <56960143+anjub95@users.noreply.github.com>
2019-10-25 10:08:56 +0530

    Update demoTool.groovy

--- vars/demoTool.groovy
+++ vars/demoTool.groovy
@@ -21,7 +21,7 @@ def downloadArtifact(config, toolsArtifactPath){
     pwd
     //sh 'wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz'
         def out = 'tools/'
-    sh 'curl -L -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -o' + out
+    sh 'curl -L -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -o ' + out
         /* def downloadSpec = JsonOutput.toJson([
             files: [
                     [
