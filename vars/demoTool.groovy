030e908805612b23b7ba88dfaa486235c0d45c89
anjub95 <56960143+anjub95@users.noreply.github.com>
2019-10-28 13:00:00 +0530

    changing abspath from /usr/bin

--- vars/demoTool.groovy
+++ vars/demoTool.groovy
@@ -21,10 +21,10 @@ def downloadArtifact(config, toolsArtifactPath){
     def out = './tools/apache-maven'
         sh 'pwd'
         sh 'mkdir -p ./tools/apache-maven'
+        sh 'mkdir ./maven_extract'
         //sh 'file ./tools/oracle-jdk/jdk-8u131-linux-x64.tar.gz'
         sh 'pwd'
         sh 'ls -ltr /var/lib/jenkins/workspace/SharedLibrary_pipeline/tools/apache-maven/apache-maven-3.3.9-bin.tar.gz'
-        sh 'ls -ltr /usr/bin'
     //sh 'wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P '+ out
        sh 'wget -nc https://repo.maven.apache.org/maven2/org/apache/maven/apache-maven/3.3.9/apache-maven-3.3.9-bin.tar.gz -P '+ out 
        sh 'chmod 777 ./tools/apache-maven/apache-maven-3.3.9-bin.tar.gz'
@@ -67,7 +67,7 @@ def downloadInstallTool(config, toolName){
  */
 def untarDownloadedFile(relPathWithfileName){
    def absPathWithFileName = env.WORKSPACE + '/' + relPathWithfileName
-   def absPath = '/usr/bin'
+   def absPath = '/var/lib/jenkins/workspace/SharedLibrary_pipeline/maven_extract'
    def toolFolderName = sh (returnStdout: true, script:  """
                             FOLDER_NAME=`tar tzf "${absPathWithFileName}" | sed -e 's@/.*@@' | uniq`
                             tar -zxf /var/lib/jenkins/workspace/SharedLibrary_pipeline/tools/apache-maven/apache-maven-3.3.9-bin.tar.gz -C ${absPath}
