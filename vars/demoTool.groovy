05e7268660fc49030ff0ced97a3d6355c9d2e626
anjub95 <56960143+anjub95@users.noreply.github.com>
2019-10-28 12:39:00 +0530

    ./ in cd command

--- vars/demoTool.groovy
+++ vars/demoTool.groovy
@@ -22,7 +22,7 @@ def downloadArtifact(config, toolsArtifactPath){
         sh 'pwd'
         sh 'mkdir -p ./tools/apache-maven'
         //sh 'file ./tools/oracle-jdk/jdk-8u131-linux-x64.tar.gz'
-        sh 'cd tools/apache-maven'
+        sh 'cd ./tools/apache-maven'
         sh 'pwd'
         sh 'ls -ltr'
     //sh 'wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz -P '+ out
